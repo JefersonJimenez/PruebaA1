ea35b21d22e90c2dbd4001189cb999eef0aae114
JefersonJimenez <jefersonjimenezpardo@gmail.com>
2022-09-22 11:02:44 -0400

    pruebaA

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PieComponent } from './pie/pie.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    PieComponent,
+    MenuComponent,
+    ContenidoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
